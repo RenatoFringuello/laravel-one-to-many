d8ca8fcad028243505992ec97439fe46c0987fc9
RenatoFringuello <renatofringuello@hotmail.it>
2023-02-28 18:46:16 +0100

    add after for column

--- database/migrations/2023_02_24_155724_add_image_col_to_projects_table.php
+++ database/migrations/2023_02_24_155724_add_image_col_to_projects_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::table('projects', function (Blueprint $table) {
             //
-            $table->text('image');
+            $table->text('image')->after('content');
         });
     }
 
